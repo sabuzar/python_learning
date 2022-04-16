625e82344fe861e97c527afd7685472e76e3067e
Abu Zar <103764167+sabuzar@users.noreply.github.com>
2022-04-16 21:03:08 +0500

    learning for loop

---
+++ for_loop.py
@@ -0,0 +1,11 @@
+for i in range(0,5):
+    if i == 0:
+        print("*")
+    if i == 1:
+        print("**")
+    if i == 2:
+        print("***")
+    if i == 3:
+        print("****")
+    if i == 4:
+        print("*****")
